e8c052e7047d4c40af71084166e28eb3ec9ec12c
Tay M Huval <taylor.mleigh@gmail.com>
2020-01-28 12:28:52 -0600

    Delete config.js

--- database-mysql/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  user: 'student', 
-  // if you are using the pairing stations, the user is `student`
-  password: 'student', 
-  // if you are using the paring stations, the password is `student`
-  // if you do not have a password, delete this key/value pair from
-  // the object
-  database: 'search'
-};
